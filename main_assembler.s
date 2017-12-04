a7abc5a2b529656deed4749f6fd6830b817dad73
EiE <eie@embeddedinembedded.com>
2017-12-04 14:31:47 -0700

    Update project file layout

--- main_assembler.s
+++ main_assembler.s
@@ -5,7 +5,6 @@
 ;------------------------------------------------------------------------------
 ; Description
 ; This is a demonstration program to show basic code structures in assembly
-; A test comment.
 
 ; Includes
 #define  __ASSEMBLY__           1  ; Need this so the correct values in AT91SAM3U4.h are selected
@@ -21,5 +20,6 @@
 				
 
 main
-	
+
+
 	END
