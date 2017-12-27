772081ec29a7549d17bedec82a9def984d986718
EiE <eie@embeddedinembedded.com>
2017-12-27 15:50:06 -0700

    Update starting files

--- firmware_common/drivers/leds.c
+++ firmware_common/drivers/leds.c
@@ -28,7 +28,7 @@ PROTECTED FUNCTIONS
 
 /***********************************************************************************************************************
 Global variable definitions with scope across entire project.
-All Global variable names shall start with "G_xxLed"
+All Global variable names shall start with "G_<type>Led"
 ***********************************************************************************************************************/
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* New variables (all shall start with G_xxLed*/
@@ -44,8 +44,10 @@ extern volatile u32 G_u32ApplicationFlags;             /*!< @brief From main.c *
 
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
-Variable names shall start with "Led_" and be declared as static.
+Variable names shall start with "Led_<type>" and be declared as static.
 ***********************************************************************************************************************/
+static fnCode_type Led_StateMachine;                   /*!< @brief The state machine function pointer */
+//static u32 Led_u32Timeout;                           /*!< @brief Timeout counter used across states */
 
  
 
@@ -77,9 +79,40 @@ Promises:
 */
 void LedInitialize(void)
 {
+  /* If good initialization, set state to Idle */
+  if( 1 )
+  {
+    Led_StateMachine = LedSM_Idle;
+  }
+  else
+  {
+    /* The task isn't properly initialized, so shut it down and don't run */
+    Led_StateMachine = LedSM_Error;
+  }
   
 } /* end LedInitialize() */
 
+
+/*!----------------------------------------------------------------------------------------------------------------------
+@fn void LedRunActiveState(void)
+
+@brief Selects and runs one iteration of the current state in the state machine.
+
+All state machines have a TOTAL of 1ms to execute, so on average n state machines
+may take 1ms / n to execute.
+
+Requires:
+- State machine function pointer points at current state
+
+Promises:
+- Calls the function to pointed by the state machine function pointer
+
+*/
+void LedRunActiveState(void)
+{
+  Led_StateMachine();
+
+} /* end LedRunActiveState */
 
 
 
@@ -92,8 +125,26 @@ void LedInitialize(void)
 State Machine Declarations
 ***********************************************************************************************************************/
 
+/*!-------------------------------------------------------------------------------------------------------------------
+@fn static void LedSM_Idle(void)
+
+@brief What does this function do?
+*/
+static void LedSM_Idle(void)
+{
+ 
+} /* end LedSM_Idle() */
 
 
+/*!-------------------------------------------------------------------------------------------------------------------
+@fn static void LedSM_Error(void)
+
+@brief Handle an error here.  For now, the task is just held in this state. 
+*/
+static void LedSM_Error(void)          
+{
+  
+} /* end LedSM_Error() */
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* End of File */
