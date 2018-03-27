93b94ca0bb228e9c84f75bc7d184ea04eb05aa20
EiE <eie@embeddedinembedded.com>
2018-03-26 23:58:52 -0600

    UART WIP

--- firmware_common/drivers/utilities.h
+++ firmware_common/drivers/utilities.h
@@ -16,6 +16,13 @@ Type Definitions
 /***********************************************************************************************************************
 Constants / Definitions
 ***********************************************************************************************************************/
+#define NUMBER_ASCII_TO_DEC     (u8)48        /* Difference between ASCII character 0-9 and the value 0-9 */
+#define UPPERCASE_ASCII_TO_DEC  (u8)55        /* Difference between ASCII character A-F and the value 10-15 */
+#define LOWERCASE_ASCII_TO_DEC  (u8)87        /* Difference between ASCII character a-f and the value 10-15 */
+
+#define ASCII_CARRIAGE_RETURN   (u8)0x0D      /* ASCII CR char \r */
+#define ASCII_LINEFEED          (u8)0x0A      /* ASCII LF char \n */
+#define ASCII_BACKSPACE         (u8)0x08      /* ASCII Backspace char */
 
 
 /***********************************************************************************************************************
@@ -27,6 +34,12 @@ Constants / Definitions
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 bool IsTimeUp(u32 *pu32SavedTick_, u32 u32Period_);
+
+u8 ASCIIHexCharToNum(u8);
+u8 HexToASCIICharUpper(u8 u8Char_);
+u8 HexToASCIICharLower(u8 u8Char_);
+u8 NumberToAscii(u32 u32Number_, u8* pu8AsciiString_);
+bool SearchString(u8* pu8TargetString_, u8* pu8MatchString_);
 
 
 /*--------------------------------------------------------------------------------------------------------------------*/
